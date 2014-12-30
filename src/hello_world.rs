9162a0925cc2f70fee9a97bed52ff3030d32246c
Alexandru Bordei <alexandru.bordei@hostway.ro>
2014-12-31 01:33:48 +0200

    Added files

---
+++ src/hello_world.rs
@@ -0,0 +1,11 @@
+#![feature(globs)]
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+    Iron::new(|&: _: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello world!")))
+    }).listen("localhost:3000").unwrap();
+}
